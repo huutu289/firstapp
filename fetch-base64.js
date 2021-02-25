d2900d08e1c742d162bce2a387db165b9d79613f
CHTU_STTTT\Admin <huutu289@gmail.com>
2021-02-25 08:58:10 +0700

    done

--- fetch-base64.js
+++ fetch-base64.js
@@ -1,3 +1,4 @@
+
 const fetchAsBlob = url => fetch(url)
     .then(response => response.blob());
 
@@ -11,19 +12,72 @@ const convertBlobToBase64 = blob => new Promise((resolve, reject) => {
 });
 
 
-function start() {    
-    var list = document.getElementsByTagName('img');
+
+function start() {
+    let list = document.getElementsByTagName('img');
+    let count = list.length;
+    console.log(count);
+
     for (let item of list) {
-        let url = item.getAttribute('data-src');       
-        fetchAsBlob(url)
-            .then(convertBlobToBase64)
-            .then(
-                function (data) {            
-                                  
-                    item.setAttribute('src', data);
-                }
-            )
+        let url = item.getAttribute('data-src');
+
+        //check if url is base64
+        const checkBase64 = url.includes('data:image');
+        if (checkBase64) {
+            item.setAttribute('src', url);
+
+            item.removeAttribute('data-src');
+            
+            //del icon loading
+            let parentNode = item.parentElement;
+            let loadIcon = parentNode.firstElementChild;
+            loadIcon.remove();
+            count--;
+            turnOff(count);
+        }
+
+        else {
+
+            if (url != null) {
+                // console.log(url);
+                fetchAsBlob(url)
+                    .then(convertBlobToBase64)
+                    .then(
+                        function (data) {
+                            item.setAttribute('src', data);
+                            item.removeAttribute('data-src');
+                            let parentNode = item.parentElement;
+                            let loadIcon = parentNode.firstElementChild;
+
+                            loadIcon.remove();
+
+                            count--;
+                            turnOff(count);
+                        }
+                    )
+            }
+        }
+
+        
     }
+
+
+
+
 }
-let btnElement = document.querySelector('#getImg');
-btnElement.addEventListener('click', start);
+start();
+
+function turnOff(count) {
+    console.log(count);
+    if (count === 0) {
+        let status = document.querySelector('#load');
+        console.log(status);
+        status.remove();
+
+    }
+
+
+}
+
+// let btnElement = document.querySelector('#getImg');
+// btnElement.addEventListener('click', start);
